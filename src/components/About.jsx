1aff549153ffc49d1293ed232a8b49f94140b411
Suresh <mail2sghimire@gmail.com>
2022-07-21 03:09:28 -0700

    sign up link added on get started btn

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ function About() {
               type="button"
               class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
             >
-              Get started
+              <a href="signup">Get started</a>
             </button>
           </div>
         </div>
